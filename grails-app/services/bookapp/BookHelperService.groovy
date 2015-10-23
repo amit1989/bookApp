db8f22038c66d3f41c21b5b6b228c76ecb1290f4
amit_koranne <amit.koranne@gmail.com>
2015-10-23 16:27:11 +0530

    Wishlist

--- grails-app/services/bookapp/BookHelperService.groovy
+++ grails-app/services/bookapp/BookHelperService.groovy
@@ -1,6 +1,7 @@
 package bookapp
 
 import grails.transaction.Transactional
+import org.json.simple.JSONObject
 
 @Transactional
 class BookHelperService {
@@ -39,5 +40,34 @@ class BookHelperService {
 
         return jsonMap;
 
+    }
+
+    public JSONObject getBookAsJson(Book books){
+
+        JSONObject object = new JSONObject();
+
+        object.put("id", books.id)
+        object.put("title", books.title)
+        object.put("isbn", books.isbn)
+        object.put("discreption", books.discreption)
+        object.put("author", books.author)
+        object.put("image_url", books.url)
+        object.put("category", books.category.name)
+        object.put("isShared", books.isShared)
+        object.put("isOnSell", books.isOnSell)
+        object.put("startDate", books.startDate)
+        object.put("isDonated", books.isDonated)
+        object.put("endDate", books.endDate)
+        object.put( "originalCost", books.originalCost)
+        object.put("discount", books.discount)
+        object.put("bookEdition", books.bookEdition)
+        object.put("yearOfBook" , books.yearOfBook)
+        object.put("isCompleted", books.isCompleted)
+        object.put("onCondition", books.onCondition)
+        object.put("dateCreated", books.dateCreated)
+        object.put("userName", books.user?.userName)
+        object.put("userEmail", books.user?.email)
+
+        return object;
     }
 }
